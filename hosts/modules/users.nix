f82ce42a19f612fcfbf03e21fe4dcf714465c9a5
Modgamedev <modgamedev@gmail.com>
2025-11-19 03:00:51 +0500

    Update users.nix

--- hosts/modules/users.nix
+++ hosts/modules/users.nix
@@ -1,16 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  users.users.mod = {                     # Имя пользователя mod
-    enable = true;                        # Включаем пользователя
-    isNormalUser = true;                  # Обычный пользователь, не системный
-    createHome = true;                    # Создаем домашнюю директорию с правильными правами
-    extraGroups = [ "wheel" ];            # Даем права sudo
-    home = "/home/mod";                   # Путь домашней директории
-    homeMode = "700";                     # Права на домашнюю директорию только для пользователя
-    hashedPassword = "";                  # TODO: решить вопрос с паролем
-    openssh.authorizedKeys.keys = [       # Публичный SSH-ключ
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIB7Tz/5egH9CNwVK30Mq6fvvrxD8y/DIMV7ty/lcgxt"
-    ];
+  users = {
+    allowNoPasswordLogin = false;             # Оставляем проверку, что есть возможность войти по паролю или SSH-ключу админу
+    defaultUserHome = "/home";                # Директория для обычного пользователя по умолчанию
+    defaultUserShell = pkgs.bash;             # Оболочка по умолчанию
+    mutableUsers = false;                     # Все пользователи и группы ТОЛЬКО через конфиг
+    users = {
+      mod = {                                 # Имя пользователя mod
+        enable = true;                        # Включаем пользователя
+        isNormalUser = true;                  # Обычный пользователь, не системный
+        createHome = true;                    # Создаем домашнюю директорию с правильными правами
+        home = "/home/mod";                   # Путь домашней директории
+        homeMode = "700";                     # Права на домашнюю директорию только для пользователя
+        extraGroups = [ "wheel" ];            # Даем права sudo
+        useDefaultShell = true;               # Использовать оболочку по умолчанию
+        TODO password
+        openssh.authorizedKeys.keys = [       # Публичный SSH-ключ
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIB7Tz/5egH9CNwVK30Mq6fvvrxD8y/DIMV7ty/lcgxt"
+        ];
+      };
+    };
   };
 }
