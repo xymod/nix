e2c7757547f31a58c9f8ce11578ebe248d6911b1
Modgamedev <modgamedev@gmail.com>
2025-11-19 20:20:12 +0500

    Update laptop.nix

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -5,6 +5,7 @@
     ./hardware-configuration.nix        # конфиг разделов диска и файловых систем
     ./modules/users.nix                 # модуль пользователя
     ./modules/systemd-boot.nix          # модуль загрузчика systemd-boot
+    ./modules/tty.nix                   # настройки tty шрифта, приветствия
   ];
 
   networking.hostName = "nixos-laptop";
