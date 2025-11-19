550152ec03cf058dd12c7ecaa52e0f4f10febe61
Modgamedev <modgamedev@gmail.com>
2025-11-19 16:31:19 +0500

    Update systemd-boot.nix

--- hosts/modules/systemd-boot.nix
+++ hosts/modules/systemd-boot.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.systemd-boot = {
-    enable = true;
+  boot = {
+    consoleLogLevel = 0;              # Выводить в консоль при загрузке только критичные сообщения
+    loader = {
+      timeout = 3;                    # Длительность показа консоли
+      systemd-boot = {                # Определяем какой загрузчик
+        enable = true;                # Включаем загрузчик systemd-boot
+        editor = false;               # Отключаем редактирование командной строки ядра перед загрузкой
+        configurationLimit = 20;      # Сколько поколений отображается при загрузке
+        consoleMode = "max";          # ставим максимальное разрешение консоли
+      };
+    };
   };
 }
