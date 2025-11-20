7f7240ce92641be2960518840cfb53d521980b2c
Modgamedev <modgamedev@gmail.com>
2025-11-20 16:32:50 +0500

    Update laptop.nix

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -9,6 +9,7 @@
     ./modules/network-iwd.nix           # только wi-fi
   ];
 
+  programs.nano.enable = false;
   networking.hostName = "nixos-laptop";
   system.stateVersion = "25.05";
   time.timeZone = "UTC";
