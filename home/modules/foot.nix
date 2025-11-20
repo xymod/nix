8817243b038109a547b25290d4fd090e1fc38dda
Modgamedev <modgamedev@gmail.com>
2025-11-21 00:40:22 +0500

    Update foot.nix

--- home/modules/foot.nix
+++ home/modules/foot.nix
@@ -3,5 +3,24 @@
 {
   programs.foot = {
     enable = true;
+    settings = {
+      main = {
+        "dpi-aware" = "yes";
+      };
+
+      cursor = {
+        style = "beam";
+        "unfocused-style" = "none";
+        blink = "yes";
+        "blink-rate" = 500;
+        "beam-thickness" = 1.0;
+      };
+
+      scrollback = {
+        lines = 1000;
+        multiplier = 8.0;
+        "indicator-position" = "relative";
+      };
+    };
   };
 }
