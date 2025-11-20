eee4d0de89103433dfe720153c12e7aa22aa8a31
Modgamedev <modgamedev@gmail.com>
2025-11-20 17:49:19 +0500

    Update systemd-boot.nix

--- hosts/modules/systemd-boot.nix
+++ hosts/modules/systemd-boot.nix
@@ -16,7 +16,7 @@
       systemd-boot = {                    # Определяем какой загрузчик
         enable = true;                    # Включаем загрузчик systemd-boot
         editor = false;                   # Отключаем редактирование командной строки ядра перед загрузкой
-        configurationLimit = 20;          # Сколько поколений отображается при загрузке
+        configurationLimit = 30;          # Сколько поколений отображается при загрузке
         consoleMode = "max";              # ставим максимальное разрешение консоли
       };
       efi.canTouchEfiVariables = true;    # Разрешаем NixOS управлять EFI-загрузочными записями
