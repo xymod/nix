07e1a339c1ec57b6b63bd61aa60e9fbfedf62cf3
Modgamedev <modgamedev@gmail.com>
2025-11-21 03:47:32 +0500

    Update zsh.nix

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -5,14 +5,24 @@
     enable = true;              # Включено
     defaultKeymap = "viins";    # insert mode по умолчанию, Esc → normal mode
     enableCompletion = true;    # Автодополнение
+    
+    # История
     history = {
       append = true;            # Дописывать историю, а не перезаписывать
       share = true;             # Общий пул истории между сессиями
       saveNoDups = true;        # Не сохранять дубликаты
     };
+
+    # Подсветка синтаксиса
     syntaxHighlighting = {
       enable = true;                            # Включено
       highlighters = [ "main" "brackets" ];     # Что подсвечивать
+    };
+
+    # Автоподсказкии по истории
+    autosuggestion = {
+      enable = true;                            # Включено
+      highlight =  "fg=#00ffff";                # Стиль автоподсказок
     };
   };
 }
