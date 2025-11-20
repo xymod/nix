43383b933d6ede169b8060da26d3be5df2a1d1dc
Modgamedev <modgamedev@gmail.com>
2025-11-20 21:57:20 +0500

    Update users.nix

--- hosts/modules/users.nix
+++ hosts/modules/users.nix
@@ -11,11 +11,11 @@
         enable = true;                        # Включаем пользователя
         isNormalUser = true;                  # Обычный пользователь, не системный
         createHome = true;                    # Создаем домашнюю директорию с правильными правами
-        home = "/home/mod";                   # Путь домашней директории
+        home = "/home/mod";                   # Путь домашней директории (не обязательно)
         homeMode = "700";                     # Права на домашнюю директорию только для пользователя
         extraGroups = [ "wheel" ];            # Даем права sudo
         useDefaultShell = true;               # Использовать оболочку по умолчанию
-        TODO password
+        password = "123";                     # TODO sops
         openssh.authorizedKeys.keys = [       # Публичный SSH-ключ
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIB7Tz/5egH9CNwVK30Mq6fvvrxD8y/DIMV7ty/lcgxt"
         ];
