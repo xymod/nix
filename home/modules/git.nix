44cb950b9d4a57a96476cfeb5a3c805a77a4791a
xymod <modgamedev@gmail.com>
2025-11-21 23:14:05 +0500

    Update git.nix

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -3,5 +3,10 @@
 {
   programs.git = {
     enable = true;
+    signing = {
+      format = "ssh";                  # Метод подписи коммитов и тегов
+      signByDefault = true;            # Подписывать все коммиты и теги по умолчанию
+      key = "/media/usb/ssh/github";   # Путь к приватному ключу
+    };
   };
 }
