29a1c49588792b70dc803fd557382324a42251bb
xymod <modgamedev@gmail.com>
2025-11-22 16:41:37 +0500

    Update waybar.nix

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -3,6 +3,22 @@
 {
   programs.waybar = {
     enable = true;
+	style = ''
+	  * { all: unset; }
+      /* Общее для всех */
+      window#waybar {
+        font-family: "Iosevka NF Light";
+        font-size: 30px;
+        color: #E9E9E9;
+      }
+	  /* Верхний бар */
+	  window#waybar.top {
+    	background: rgba(32, 32, 32, 0.99);
+    	border: 2px solid #575757;
+    	border-top: none;
+    	border-radius: 0 0 35px 35px;  /* верх-лево | верх-право | низ-право | низ-лево */
+	  }
+	'';
     settings = {
       mainBar = {
         position = "top";
