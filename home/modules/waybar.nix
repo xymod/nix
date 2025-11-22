291a6c473dffbeab99fe307c134820bafe83adbf
xymod <modgamedev@gmail.com>
2025-11-22 15:01:02 +0500

    Update waybar.nix

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -6,7 +6,20 @@
     settings = {
       mainBar = {
         position = "top";
-        height = 25;
+        height = 50;
+
+      "modules-left": ["cpu", "memory", "temperature", "disk", "battery", "network"];
+      "modules-center": ["custom/active-windows"];
+      "modules-right": ["niri/language";
+
+      "cpu": {
+	      interval: 10;
+	      format: "  {usage}%";
+	      states: {
+	        medium: 50;
+	        high: 70;
+	        critical: 90
+	      };
       };
     };
   };
