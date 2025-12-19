9dcd6a00cb27ec09d14abd0b3c46a71543e19365
xymod <modgamedev@gmail.com>
2025-12-19 09:00:16 +0000

    Create greetd.nix

---
+++ hosts/modules/greetd.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [ tuigreet ];
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --cmd niri-session";
+      };
+    };
+  };
+}
