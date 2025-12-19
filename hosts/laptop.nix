25272fcfdf9724db30895a82a9f52d22549aa69d
xymod <modgamedev@gmail.com>
2025-12-19 09:02:16 +0000

    Update laptop.nix

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,20 +1,46 @@
 { config, lib, pkgs, ... }:
 
 {
+  #######################
+  ### Модули системы  ###
+  #######################
   imports = [
-    ./modules/hardware-laptop.nix       # железо
-    ./modules/users.nix                 # модуль пользователя
-    ./modules/systemd-boot.nix          # модуль загрузчика systemd-boot
-    ./modules/tty.nix                   # настройки tty шрифта, приветствия
-    ./modules/network-iwd.nix           # только wi-fi
+    ./modules/hardware-laptop.nix       # Настройки железа
+    ./modules/users.nix                 # Пользователеи, группы, права
+    ./modules/systemd-boot.nix          # UEFI загрузчик systemd-boot
+    ./modules/tty.nix                   # TTY: шрифт, приветствие, автологин
+    ./modules/network-iwd.nix           # Wi-Fi
+    ./modules/greetd.nix
   ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  environment.systemPackages = with pkgs; [ brightnessctl ];
-  xdg.portal.enable = false;
-  programs.niri.enable = true;
-  programs.nano.enable = false;
-  networking.hostName = "nixos-laptop";
-  system.stateVersion = "25.05";
-  time.timeZone = "UTC";
-  services.udisks2.enable = true;
+
+  #######################
+  ### Общие настройки ###
+  #######################
+  networking.hostName = "nixos-laptop";                              # Имя хоста системы
+  system.stateVersion = "25.05";                                     # Версия NixOS (НЕ обновлять без причины)
+  time.timeZone = "UTC";                                             # Часовой пояс
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];   # Включаем flakes и nix-command
+  #xdg.portal.enable = false;                                         #
+
+  #######################
+  ###     Утилиты     ###
+  #######################
+  environment.systemPackages = with pkgs; [ 
+    brightnessctl                                                    # Управление яркостью экрана
+  ];
+
+  #######################
+  ###    Программы    ###
+  #######################
+  programs = {
+    niri.enable = true;                                              # Включаем Wayland-композитор niri
+    nano.enable = false;                                             # Отключаем nano (используем другой редактор)
+  };
+
+  #######################
+  ###    Сервисы      ###
+  #######################
+  services = {
+    udisks2.enable = true;                                           # Включаем автомонтирование съёмных носителей
+  };
 }
