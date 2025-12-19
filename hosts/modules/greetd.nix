df6b7fae3811e29f339c84d907e043829e18bbd0
xymod <modgamedev@gmail.com>
2025-12-19 10:14:29 +0000

    Update greetd.nix

--- hosts/modules/greetd.nix
+++ hosts/modules/greetd.nix
@@ -2,11 +2,20 @@
 
 {
   services.greetd = {
+    #######################
+    ### Общие настройки ###
+    #######################
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
         user = "mod";
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+          --time \
+          --asterisks \
+          --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' \
+          --cmd niri-session
+        '';
       };
     };
   };
