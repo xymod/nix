e2c9cb66930e1c1c42accb05e2d0fe62ccb47d8d
xymod <modgamedev@gmail.com>
2025-12-19 11:24:07 +0000

    Update greetd.nix

--- hosts/modules/greetd.nix
+++ hosts/modules/greetd.nix
@@ -5,17 +5,20 @@
     #######################
     ### Общие настройки ###
     #######################
-    enable = true;
-    settings = {
-      default_session = {
-        command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --time \                                         # Показывать время
-          --asterisks \                                    # Показывать звездочки при вводе пароля
-          --cmd niri-session                               # Команда, которая запускается после логина
-          --theme 'border=blue;text=white;prompt=green;time=magenta;action=cyan;button=yellow;container=black;input=red' \
-        '';
-      };
-    };
+    enable = true;                                        # Включаем greetd
+
+    ###################################
+    ### Настройки tuigreet и сессии ###
+    ###################################
+    settings.default_session.command = ''
+      ${pkgs.greetd.tuigreet}/bin/tuigreet \              # Запуск tui-версии greetd (tuigreet) — текстовый greeter
+      --time \                                            # Показывать время
+      --time-format "%d.%m.%Y %H:%M" \                    # Формат даты и времени
+      --power-shutdown "systemctl poweroff" \             # Команда для выключения системы
+      --power-reboot "systemctl reboot" \                 # Команда для перезагрузки системы
+      --asterisks \                                       # Отображать звездочки при вводе пароля
+      --theme 'border=blue;text=white;prompt=green;time=magenta;action=cyan;button=yellow;container=black;input=red' \
+      --cmd niri-session                                  # Команда, которая запускается после логина
+    '';
   };
 }
