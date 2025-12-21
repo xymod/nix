32311e41c36c875158a5baef1aa0400beffc7cc4
xymod <modgamedev@gmail.com>
2025-12-21 15:00:21 +0000

    Update greetd.nix

--- hosts/modules/greetd.nix
+++ hosts/modules/greetd.nix
@@ -11,14 +11,13 @@
     ### Настройки tuigreet и сессии ###
     ###################################
     settings.default_session.command = ''
-      ${pkgs.greetd.tuigreet}/bin/tuigreet \              # Запуск tui-версии greetd (tuigreet) — текстовый greeter
-      --time \                                            # Показывать время
-      --time-format "%d.%m.%Y %H:%M" \                    # Формат даты и времени
-      --power-shutdown "systemctl poweroff" \             # Команда для выключения системы
-      --power-reboot "systemctl reboot" \                 # Команда для перезагрузки системы
-      --asterisks \                                       # Отображать звездочки при вводе пароля
-      --theme 'border=blue;text=white;prompt=green;time=magenta;action=cyan;button=yellow;container=black;input=red' \
-      --cmd niri-session                                  # Команда, которая запускается после логина
+      ${pkgs.greetd.tuigreet}/bin/tuigreet \
+      --time \
+      --time-format "%d.%m.%Y %H:%M" \
+      --asterisks \
+      --cmd niri-session \
+      --theme 'border=blue;text=white;prompt=green;time=magenta;action=cyan;button=yellow;container=black;input=red'
     '';
   };
 }
+
