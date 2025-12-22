bf97d4c15feb01c3db4f515e9541d8bc56358323
mod <dmitrii@example.com>
2025-12-22 08:08:15 +0000

    git update

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -2,14 +2,23 @@
 
 {
   programs.git = {
-    enable = true;
-    userName = "mod";
-    userEmail = "dmitrii@example.com";
+    #######################
+    ### Общие настройки ###
+    #######################
+    enable = true;                                      # Включаем git
+    userName = "mod";                                   # Имя пользователя
+    userEmail = "mod@example.com";                      # Почта пользователя
+    #######################
+    ###     Подпись     ###
+    #######################
     signing = {
-      format = "ssh";                  # Метод подписи коммитов и тегов
-      signByDefault = true;            # Подписывать все коммиты и теги по умолчанию
+      format = "ssh";                                   # Метод подписи коммитов и тегов
+      signByDefault = true;                             # Подписывать все коммиты и теги по умолчанию
       key = "/run/media/mod/SecureVault/.ssh/github";   # Путь к приватному ключу
     };
+    #######################
+    ###  Доп настройки  ###
+    #######################
     extraConfig = ''
       [core]
       sshCommand = "ssh -i /run/media/mod/SecureVault/.ssh/github -F /dev/null";
