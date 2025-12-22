cccd22871e87d7c26a7c3021cbfe9df0b94caaa5
xymod <modgamedev@gmail.com>
2025-12-22 07:16:12 +0000

    Update git.nix

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -3,10 +3,18 @@
 {
   programs.git = {
     enable = true;
+    userName = "mod";
+    userEmail = "dmitrii@example.com";
     signing = {
       format = "ssh";                  # Метод подписи коммитов и тегов
       signByDefault = true;            # Подписывать все коммиты и теги по умолчанию
-      key = "/media/usb/ssh/github";   # Путь к приватному ключу
+      key = "/run/media/mod/SecureVault/.ssh/github";   # Путь к приватному ключу
     };
+    extraConfig = ''
+      [core]
+      sshCommand = "ssh -i /run/media/mod/SecureVault/.ssh/github -F /dev/null";
+      [url "git@github.com:xymod/"]
+      insteadOf = "https://github.com/xymod/"
+    '';
   };
 }
